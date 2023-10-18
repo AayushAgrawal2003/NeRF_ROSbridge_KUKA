c81e463e2171eb2184de2c2e965f1cf0419afe74
AayushAgrawal2003 <aayush.agrawal2003@gmail.com>
2023-10-18 13:14:46 +0530

    matrix

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,25 +6,40 @@ import tf2_ros
 import PyKDL
 import geometry_msgs.msg
 from math import pi
+import numpy as np
+from tf import transformations
+
+from tf.transformations import translation_matrix, quaternion_matrix
 
 
-if __name__ == '__main__':
-    rospy.init_node('tf_echo')
 
+
+
+def get_camera_transform():
+    rospy.init_node('tf_echo_node')
     tfbuffer = tf2_ros.Buffer()
-
     listener = tf2_ros.TransformListener(tfbuffer)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
-            trans = tfbuffer.lookup_transform('base_link', 'iiwa_link_7', rospy.Time(0) , rospy.Duration(1))
-            print(trans)
-            trans = trans.transform
-            rot = PyKDL.Rotation.Quaternion(* [ eval('trans.rotation.'+c) for c in 'xyzw'] )
-            print(' '.join( [ str(eval('trans.rotation.'+c)) for c in 'xyzw'] ))
-            ypr = [ i  / pi * 180 for i in rot.GetEulerZYX() ]
+            transform_msg = tfbuffer.lookup_transform('base_link', 'iiwa_link_7', rospy.Time(0) , rospy.Duration(1))
+            translation = [transform_msg.transform.translation.x, transform_msg.transform.translation.y, transform_msg.transform.translation.z]
+            rotation = [transform_msg.transform.rotation.x, transform_msg.transform.rotation.y, transform_msg.transform.rotation.z, transform_msg.transform.rotation.w]
 
+            # Create a 4x4 translation matrix
+            matrix = translation_matrix(translation)
+
+            # Create a 4x4 rotation matrix from the quaternion
+            rotation_matrix = quaternion_matrix(rotation)
+
+            # Combine the translation and rotation matrices to get the 4x4 transformation matrix
+            transformation_matrix = np.dot(matrix, rotation_matrix)
+
+            print(transformation_matrix)
+            break
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
             print("Fail", e)
     
-        rate.sleep()
+        rate.sleep()   
+
+get_camera_transform()
